9f229f88efae04615413992eac007186d6a34923
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-07-15 14:29:50 +0200

    Awesome

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,6 +2,9 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
 
 import qualified Data.Map as Map
 
@@ -15,4 +18,6 @@ baseConfig = desktopConfig
 myConfig conf@(XConfig {XMonad.modMask = modMask}) = conf `additionalKeys`
     [ ((XMonad.modMask conf .|. shiftMask, xK_l), spawn "slock")
     , ((XMonad.modMask conf .|. shiftMask, xK_Return), spawn $ (XMonad.terminal conf) ++ " -e bash --rcfile ~/.profile")
+    , ((XMonad.modMask conf, xK_p), shellPrompt defaultXPConfig)
+    , ((XMonad.modMask conf, xK_l), xmonadPrompt defaultXPConfig)
     ]
